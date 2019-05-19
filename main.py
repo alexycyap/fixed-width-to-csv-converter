c5dcb97cbef775f48d2c12ddfcfd66756bb82de1
Alexander Yap <alexycyap@gmail.com>
2019-05-19 11:25:45 +1000

    Created top level FixedWidthToCsvConverter class.

--- main.py
+++ main.py
@@ -2,13 +2,26 @@ import sys
 import converter.text_converter as tc
 import spec.parser as sp
 
+class FixedWidthToCsvConverter(object):
+    def __init__(self, spec_file_path):
+        with open(spec_file_path, "r") as sf:
+            self.file_spec = sp.parse(sf.read())
+
+    def convert(self, input_file_path, output_file_path):
+        with open(input_file_path, "r", encoding = self.file_spec.input_encoding) as infile, open(output_file_path, "w", encoding = self.file_spec.output_encoding) as outfile:
+            tc.convert_text(infile, outfile, self.file_spec)
+
+
+
+
 if __name__ == '__main__':
 
     if len(sys.argv) != 4:
         sys.exit("SYNTAX: python3 main.py (spec.json file) (input file) (output file)")
 
-    with open(sys.argv[1], "r") as spec_file:
-        file_spec = sp.parse(spec_file.read())
+    spec_file_path = sys.argv[1]
+    input_file_path = sys.argv[2]
+    output_file_path = sys.argv[3]
 
-        with open(sys.argv[2], "r", encoding = file_spec.input_encoding) as infile, open(sys.argv[3], "w", encoding = file_spec.output_encoding) as outfile:
-            tc.convert_text(infile, outfile, file_spec)
+    converter = FixedWidthToCsvConverter(spec_file_path)
+    converter.convert(input_file_path, output_file_path)
